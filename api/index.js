dfc1b67c55d25a0af93eee7d934e7cb268c65731
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2020-10-19 14:59:07 +0300

    Mock FNAME resource
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const usersCurrent = require('../mockdata/api/users-current.json');
 const assetsTypes = require('../mockdata/api/assets-types.json');
 const about = require('../mockdata/api/about.json');
+const assetsSearch = require('../mockdata/api/assets-search.json');
+const omasSettings = require('../mockdata/api/omas-settings.json');
+const assetsDatabaseColumn = require('../mockdata/api/assets-database-column.json');
+const lineageEntitiesDatabaseColumn = require('../mockdata/api/lineage-entities-database-column.json');
+const lineageEntitiesDatabaseColumnVerticalLineage = require('../mockdata/api/lineage-entities-database-column-vertical-lineage.json');
+const lineageEntitiesDatabaseColumnEnd2End = require('../mockdata/api/lineage-entities-database-column-end-2-end.json');
+const lineageEntitiesDatabaseColumnUltimateSource = require('../mockdata/api/lineage-entities-database-column-ultimate-source.json');
+const lineageEntitiesDatabaseColumnSourceAndDestination = require('../mockdata/api/lineage-entities-database-column-source-and-destination.json');
 
 module.exports = function(app) {
   app.get('/api/js/global', (req, res) => {
@@ -37,5 +45,37 @@ module.exports = function(app) {
 
   app.get('/api/about', (req, res) => {
     res.json(about);
+  });
+
+  app.get('/api/assets/search', (req, res) => {
+    res.json(assetsSearch);
+  });
+
+  app.get('/api/omas/settings', (req, res) => {
+    res.json(omasSettings);
+  });
+
+  app.get('/api/assets/database_column:somedata', (req, res) => {
+    res.json(assetsDatabaseColumn);
+  });
+
+  app.get('/api/lineage/entities/database_column:somedata/ultimate-destination', (req, res) => {
+    res.json(lineageEntitiesDatabaseColumn);
+  });
+
+  app.get('/api/lineage/entities/database_column:somedata/vertical-lineage', (req, res) => {
+    res.json(lineageEntitiesDatabaseColumnVerticalLineage);
+  });
+
+  app.get('/api/lineage/entities/database_column:somedata/end2end', (req, res) => {
+    res.json(lineageEntitiesDatabaseColumnEnd2End);
+  });
+
+  app.get('/api/lineage/entities/database_column:somedata/ultimate-source', (req, res) => {
+    res.json(lineageEntitiesDatabaseColumnUltimateSource);
+  });
+
+  app.get('/api/lineage/entities/database_column:somedata/source-and-destination', (req, res) => {
+    res.json(lineageEntitiesDatabaseColumnSourceAndDestination);
   })
 };
